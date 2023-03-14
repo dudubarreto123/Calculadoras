44945872ab1647f0a77df0417c2d2199f4b862d7
Eduardo Paiva Barreto <120474364+dudubarreto123@users.noreply.github.com>
2023-03-14 10:36:05 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,27 @@
+const display = document.querySelector("#display"); // O queryselector retorna o primeiro elemento dentro do documento.
+
+const buttons = document.querySelectorAll("button"); // Retorna uma lista de elementos presentes no documento.
+
+//A declaração const cria uma variável cujo o valor é fixo, ou seja, uma constante somente leitura. Isso não significa que o valor é imutável, apenas que a variável constante não pode ser alterada ou retribuída.
+
+//=== (Valor e Tipo igual) - é usado para a comparação entre duas variáveis, mas isso irá verificar o tipo estrito, o que significa que ele irá verificar o tipo de dados e comparar dois valores.
+
+//O método slice() percorre a string ou o array para selecionar a faixa de valores determinada por parâmetro. A primeira posição de uma string ou array é a posição de número 0.
+
+//O operador de atribuição encurtado += pode ser utilizado para concatenar strings.
+
+buttons.forEach((btn) => {
+    btn.addEventListener ("click", () => {
+        if (btn.id === "=") {
+            display.value = eval(display.value);
+        } else if (btn.id === "ac") {
+            display.value = "";
+        } else if (btn.id === "de") {
+            display.value = display.value.slice(0, -1);
+
+        } else {
+            display.value += btn.id;
+        }
+    })
+})
+
